c9fc2f184edf2410552f5ec53efcceff5b02fb94
Chris Dias <105737889+chrisdias2311@users.noreply.github.com>
2023-03-12 14:12:46 +0530

    User authentication done!

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from authapp import views
 
 urlpatterns = [
-    path('', views.Home, name="Home")
+    path('', views.Home, name="Home"),
+    path('signup', views.signup, name="signup"),
+    path('login', views.handlelogin, name="handlelogin"),
+    path('logout', views.handleLogout,  name="handleLogout")
 ]
